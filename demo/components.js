a3bb138f403b153fcb4ea075d4387db2b8278889
Logan <logan.langdon@yahoo.com>
2025-09-13 20:41:04 -0700

    Finalized demo of DualComponents

---
+++ demo/components.js
@@ -0,0 +1,191 @@
+
+class CustomWidgetLight extends DualComponent {
+  constructor() {
+    const style = `
+      button {
+        outline: 4px solid white;
+        color: red;
+      }
+    `;
+    super({"shadowed": false, "shadowRootOpen": true, "style": style});
+  }
+}
+class CustomWidgetShadow extends DualComponent {
+  constructor() {
+    const style = `
+      button {
+        outline: 4px solid white;
+        color: red;
+      }
+    `;
+    super({"shadowed": true, "shadowRootOpen": true, "style": style});
+  }
+}
+class ClickerShadow extends DualComponent {
+  constructor() {
+    const style = `
+      button {
+        color: red;
+        background-color: #222;
+      }
+      clicker-wc {
+        background: white;
+      }
+    `;
+    super({"shadowed": true, "shadowRootOpen": true, "style": style});
+
+    // WC properties
+    this.count = parseInt(this.getAttribute("start")) || 0;
+    this.max = parseInt(this.getAttribute("end")) || 100;
+
+    this.button = this.root.querySelector("button");
+    this.label = this.root.querySelector("p");
+
+    this.button.addEventListener("click", this);
+  }
+  connectedCallback() {
+    this.label.textContent = this.count.toString();
+  }
+  handleEvent(event) {
+    if (event.type == "click") {
+      if (this.count < this.max) {
+        this.count += 1;
+        this.label.textContent = this.count.toString();
+      }
+    }
+  }
+}
+class ClickerLight extends DualComponent {
+  constructor() {
+    const style = `
+      button {
+        color: red;
+        background-color: #222;
+      }
+      clicker-wc {
+        background: white;
+      }
+    `;
+    super({"shadowed": false, "shadowRootOpen": true, "style": style});
+
+    // WC properties
+    this.count = parseInt(this.getAttribute("start")) || 0;
+    this.max = parseInt(this.getAttribute("end")) || 100;
+
+    this.button = this.root.querySelector("button");
+    this.label = this.root.querySelector("p");
+
+    this.button.addEventListener("click", this);
+  }
+  connectedCallback() {
+    this.label.textContent = this.count.toString();
+  }
+  handleEvent(event) {
+    if (event.type == "click") {
+      if (this.count < this.max) {
+        this.count += 1;
+        this.label.textContent = this.count.toString();
+      }
+    }
+  }
+}
+
+class SortedList extends DualComponent {
+  static observedAttributes = ["ascending", "num-items"];
+  #listItems = [];
+
+  constructor() {
+    const style = `
+      html {
+        color: white;
+        text-align: center;
+      }
+      ul {
+        margin: auto;
+        list-style: none;
+        max-width: 50%;
+      }
+      li {
+        padding: 1em;
+        margin: 0.1em;
+        background: gray;
+        border: 2px solid white;
+        text-wrap: wrap;
+        overflow-wrap: anywhere;
+      }
+      button {
+        border: 3px solid blue;
+      }
+
+    `;
+    super({"shadowed": false, "shadowRootOpen": true,  "style": style});
+    this.form = this.root.querySelector("form");
+    this.input = this.form.querySelector("input");
+    this.list = this.root.querySelector("ul");
+    this.ascendingButton = this.root.querySelector("#asc-button");
+    this.descendingButton = this.root.querySelector("#desc-button");
+
+    this.setAttribute("ascending", true);
+    this.setAttribute("num-items", 1);
+    for (const child of this.list.children) {
+      this.#listItems.push(child.textContent.trim());
+    }
+
+    this.form.addEventListener("submit", this)
+    this.ascendingButton.addEventListener("click", this);
+    this.descendingButton.addEventListener("click", this);
+
+  }
+  connectedCallback() {
+    this.sort();
+    this.render();
+  }
+  handleEvent(event) {
+    if (event.type === "submit") {
+      event.preventDefault();
+      // Add current content to listItems as a new string
+      if (this.input.value !== "") {
+        this.#listItems.push(this.input.value.trim());
+        this.setAttribute("num-items", this.#listItems.length);
+        this.input.value = "";
+      }
+    } else if (event.type === "click") {
+      if (event.target == this.ascendingButton) {
+        this.setAttribute("ascending", true)
+      } else if (event.target == this.descendingButton) {
+        this.setAttribute("ascending", false)
+      }
+    }
+  }
+  attributeChangedCallback(name, oldVal, newVal) {
+    console.log("rerender");
+    this.sort();
+    this.render();
+  }
+
+  sort() {
+    this.#listItems.sort((a,b) => {
+      const asc = this.getAttribute("ascending") === "true";
+      if(asc == true) {
+          return a.toLowerCase().localeCompare(b.toLowerCase());
+      } else {
+          return b.toLowerCase().localeCompare(a.toLowerCase());
+      }
+    });
+  }
+  render() {
+    // Update list to have an li for every element in the list property in sorted order
+    this.list.textContent = "";
+    for (const item of this.#listItems) {
+      const listItem = document.createElement("li");
+      listItem.textContent = item;
+      this.list.appendChild(listItem);
+    }
+  }
+}
+
+customElements.define("custom-widget-light", CustomWidgetLight);
+customElements.define("custom-widget-shadow", CustomWidgetShadow);
+customElements.define("clicker-wc-light", ClickerLight);
+customElements.define("clicker-wc-shadow", ClickerShadow);
+customElements.define("sorted-list", SortedList);
